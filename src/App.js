7cb2ee8816879af71b5db2c320673123a7c31bbf
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-08-14 13:28:23 +0530

    Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Home from "./pages/Home/Home";
-export default function App({children}) {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <>
-    <Home>
-      {children}
-    </Home>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
