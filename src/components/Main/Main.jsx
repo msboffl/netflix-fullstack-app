7e2ccd95ddebdb42ed172def48b7abb3e067b10a
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-08-14 15:47:57 +0530

    default setup done

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <main>
+      <section className="nf__row card-hero">
+        Unlimited movies, TV shows and more
+      </section>
+      <section className="nf__row card-animation+watchOnTv">
+        Enjoy on your TV
+      </section>
+      <section className="nf__row card-animation+downloadAndWatch">
+        Download your shows to watch offline
+      </section>
+      <section className="nf__row card-animation+watchOnDevice">
+        Watch everywhere
+      </section>
+      <section className="nf__row card-animation+kidsValueProp">
+        Create profiles for kids
+      </section>
+      <section className="nf__row nmhp-card-faq">
+        Frequently Asked Questions
+      </section>
+    </main>
+  );
+};
+
+export default Main;
