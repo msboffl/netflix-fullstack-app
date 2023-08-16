39e896f20dbae8002bd6f495d07a41476e9825ee
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-08-16 10:40:00 +0530

    Header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { NetflixBgImg } from "../../Assets/images";
 import "./Main.scss";
 
 const Main = () => {
   return (
     <main>
       <section className="nf__row card-hero">
-        Unlimited movies, TV shows and more
+        <div className="nf__card-bg">
+          <div className="card-hero__background">
+            <img src={NetflixBgImg} className="nf__bg-img" alt="Netflix Hero" />
+            <div className="card__hero-gradient"></div>
+          </div>
+        </div>
+        <div className="nf__card-text">
+          <div className="title-and-desc">
+            <h1 className="title">Unlimited movies, TV shows and more</h1>
+            <p className="desc">Watch anywhere. Cancel anytime.</p>
+          </div>
+        </div>
       </section>
       <section className="nf__row card-animation+watchOnTv">
         Enjoy on your TV
