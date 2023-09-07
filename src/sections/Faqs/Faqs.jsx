ce3146ef7b7723a5b4e025c910b8acfb39a3bdea
Mareedu Saibabu <mareedu.saibabu@publicissapient.com>
2023-09-07 11:01:33 +0530

    Faqs

--- src/sections/Faqs/Faqs.jsx
+++ src/sections/Faqs/Faqs.jsx
@@ -14,36 +14,38 @@ const faqsData = [
     id: 2,
     question: "How much does Netflix cost?",
     answer:
-      "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.",
+      "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee, Plans range from ₹ 149 to ₹ 649 a month, No extra costs, no contracts",
   },
   {
     id: 3,
     question: "Where can I watch?",
     answer:
-      "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+      "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere",
   },
   {
     id: 4,
     question: "How do I cancel?",
     answer:
-      "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+      "Netflix is flexible, There are no annoying contracts and no commitments, You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime",
   },
   {
     id: 5,
     question: "What can I watch on Netflix?",
     answer:
-      "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+      "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want",
   },
   {
     id: 6,
     question: "Is Netflix good for kids?",
     answer:
-      "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+      "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see",
   },
 ];
 
 const Accordian = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const sentences = answer.split(".");
+  // console.log(sentences);
 
   function handleToggle() {
     setIsOpen((isOpen) => !isOpen);
@@ -63,7 +65,9 @@ const Accordian = ({ question, answer }) => {
       </h3>
       {isOpen && (
         <div className="faq__answer">
-          <span>{answer}</span>
+          {sentences.map((sentence, index) => (
+            <p key={index}>{sentence.trim()}.</p>
+          ))}
         </div>
       )}
     </>
